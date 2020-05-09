09bcf854971546cb1f50c9b0af0503f87e16ff6d
Suresh Natarajan <suresh.n.natarajan@gmail.com>
2020-05-09 13:17:44 +0530

    Initial commit for product service

---
+++ src/test/java/com/suresh/practice/nova/NovaRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suresh.practice.nova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
